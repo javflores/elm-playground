8fd0ad5d0cc4c04ec9741ead60c98854632c144b
javflores <jflores@findmypast.com>
2016-06-08 21:12:13 +0100

    Adding pause button to stop subscriptions

--- src/time.elm
+++ src/time.elm
@@ -1,5 +1,6 @@
-import Html exposing(Html)
+import Html exposing(Html, div, button)
 import Html.App as App
+import Html.Events exposing (onClick)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time exposing (Time, second)
@@ -12,30 +13,39 @@ main =
         , subscriptions = subscriptions
         }
 
-type alias Model = Time
+type alias Model = 
+    { time : Time
+    , pauseTime : Bool }
 
 init : (Model, Cmd Msg)
 init = 
-    (0, Cmd.none)
+    (Model 0 False, Cmd.none)
 
 type Msg =
     Tick Time
+    | Pause
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update action model =
     case action of
         Tick newTime ->
-            (newTime, Cmd.none)
+            ({model | time = newTime}, Cmd.none)
+
+        Pause ->
+            ({model | pauseTime = not model.pauseTime}, Cmd.none)                
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Time.every second Tick
+    if model.pauseTime then
+        Sub.none
+    else
+        Time.every second Tick
 
 view : Model -> Html Msg
 view model =
     let 
         angle =
-            turns (Time.inMinutes model)
+            turns (Time.inMinutes model.time)
 
         handX =
             toString (50 + 40 * cos angle)
@@ -43,7 +53,10 @@ view model =
         handY =
             toString (50 + 40 * sin angle)
     in
-        svg [ viewBox "0 0 100 100", width "300px"]
+        div []
+        [ button [ onClick Pause ] [ text "Pause" ]
+        , svg [ viewBox "0 0 100 100", width "300px"]
             [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
             , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
             ]
+        ]
