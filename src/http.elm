326367b79c6357706bdfc5152563fdc9d0ffe977
javflores <jflores@findmypast.com>
2016-06-08 20:31:45 +0100

    Example of reading an http response to show random pictures. Time example with first usage of subscription.

--- src/http.elm
+++ src/http.elm
@@ -2,6 +2,9 @@ import Html exposing (..)
 import Html.App as Html
 import Html.Events exposing (..)
 import Html.Attributes exposing (..)
+import Http
+import Json.Decode as Json
+import Task exposing (..)
 
 main = Html.program
     { init = init,
@@ -11,21 +14,42 @@ main = Html.program
     }
 
 type alias Model = 
-    { topic : String,
-    gifUrl : String
+    { topic : String
+    , gifUrl : String
     }
 
 init : (Model, Cmd Msg)
 init =
-    (Model "cats" "waiting.gif", Cmd.none)
+    (Model "baseball" "waiting.gif", Cmd.none)
 
 type Msg = MorePlease
+    | FetchSucceed String
+    | FetchFail Http.Error
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
         MorePlease -> 
+            (model, getRandomGif model.topic)
+
+        FetchSucceed newUrl ->
+            (Model model.topic newUrl, Cmd.none)
+
+        FetchFail _ ->
             (model, Cmd.none)
+
+getRandomGif : String -> Cmd Msg
+getRandomGif topic =
+    let
+        url =
+            "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
+    in
+        Task.perform FetchFail FetchSucceed (Http.get decodeGifUrl url)
+
+decodeGifUrl : Json.Decoder String
+decodeGifUrl =
+    Json.at ["data", "image_url"] Json.string
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
