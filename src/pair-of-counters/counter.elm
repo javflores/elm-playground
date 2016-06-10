ac748f1c634ce1fca05685949d412439759f5e9c
javflores <jflores@findmypast.com>
2016-06-10 05:53:39 +0100

    Example of nesting modules counter and pair of counters

---
+++ src/pair-of-counters/counter.elm
@@ -0,0 +1,42 @@
+module Counter exposing (Model, Msg, init, update, view)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+
+type alias Model = Int
+
+init : Int -> Model
+init count =
+  count
+
+type Msg
+  = Increment
+  | Decrement
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+
+    Decrement ->
+      model - 1
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ button [ onClick Decrement ] [ text "-" ]
+    , div [ countStyle ] [ text (toString model) ]
+    , button [ onClick Increment ] [ text "+" ]
+    ]
+
+countStyle : Attribute msg
+countStyle =
+  style
+    [ ("font-size", "20px")
+    , ("font-family", "monospace")
+    , ("display", "inline-block")
+    , ("width", "50px")
+    , ("text-align", "center")
+    ]
