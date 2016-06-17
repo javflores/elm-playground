7b42461b7318aaeda87ecb152c7eb320c9d5df86
javflores <jflores@findmypast.com>
2016-06-17 21:32:41 +0100

    When incrementing or decrementing the level, sending a barch request to the api and listening for success or fail. Then updating the model.

--- src/game/src/Players/Commands.elm
+++ src/game/src/Players/Commands.elm
@@ -2,6 +2,7 @@ module Players.Commands exposing (..)
 
 import Http
 import Json.Decode as Decode exposing ((:=))
+import Json.Encode as Encode
 import Task
 import Players.Models exposing (PlayerId, Player)
 import Players.Messages exposing (..)
@@ -21,7 +22,7 @@ fetchAllUrl =
 collectionDecoder : Decode.Decoder (List Player)
 collectionDecoder =
     Decode.list memberDecoder
-    
+
 
 memberDecoder : Decode.Decoder Player
 memberDecoder =
@@ -29,3 +30,41 @@ memberDecoder =
         ("id" := Decode.int)
         ("name" := Decode.string)
         ("level" := Decode.int)
+
+saveUrl : PlayerId -> String
+saveUrl playerId =
+    "http://localhost:4000/players/" ++ (toString playerId)
+
+saveTask : Player -> Task.Task Http.Error Player
+saveTask player =
+    let
+        body =
+            memberEncoded player
+            |> Encode.encode 0
+            |> Http.string
+        config =
+            { verb = "PATCH"
+            , headers = [ ( "Content-Type", "application/json" ) ]
+            , url = saveUrl player.id
+            , body = body
+            }
+    in
+        Http.send Http.defaultSettings config
+        |> Http.fromJson memberDecoder
+
+save : Player -> Cmd Msg
+save player =
+    saveTask player
+    |> Task.perform SaveFail SaveSuccess
+
+memberEncoded : Player -> Encode.Value
+memberEncoded player =
+    let
+        list =
+            [ ( "id", Encode.int player.id )
+            , ( "name", Encode.string player.name )
+            , ( "level", Encode.int player.level )
+            ]
+    in
+        list
+        |> Encode.object
--- src/game/src/Players/Update.elm
+++ src/game/src/Players/Update.elm
@@ -3,6 +3,8 @@ module Players.Update exposing (..)
 import Navigation
 import Players.Messages exposing (Msg(..))
 import Players.Models exposing (Player)
+import Players.Commands exposing (save)
+import Players.Models exposing (Player, PlayerId)
 
 
 update : Msg -> List Player -> ( List Player, Cmd Msg )
@@ -19,3 +21,39 @@ update action players =
 
         ShowPlayer id ->
             ( players, Navigation.modifyUrl ("#players/" ++ (toString id)) )
+
+        ChangeLevel id howMuch ->
+            ( players, changeLevelCommands id howMuch players |> Cmd.batch )
+
+        SaveSuccess updatedPlayer ->
+            ( updatePlayer updatedPlayer players, Cmd.none )
+
+        SaveFail error ->
+            ( players, Cmd.none )
+
+
+
+changeLevelCommands : PlayerId -> Int -> List Player -> List (Cmd Msg)
+changeLevelCommands playerId howMuch =
+    let
+        cmdForPlayer existingPlayer =
+            if existingPlayer.id == playerId then
+                save { existingPlayer | level = existingPlayer.level + howMuch }
+
+            else
+                Cmd.none
+    in
+        List.map cmdForPlayer
+
+
+updatePlayer : Player -> List Player -> List Player
+updatePlayer updatedPlayer =
+    let
+        select existingPlayer =
+            if existingPlayer.id == updatedPlayer.id then
+                updatedPlayer
+
+            else
+                existingPlayer
+        in
+            List.map select
